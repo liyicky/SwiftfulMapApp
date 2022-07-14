d554c67eea3e76156fe2f41c61133a5ed226f7ca
liyicky <jason@liyicky.com>
2022-07-14 14:36:39 +0900

    video 3

--- SwiftfulMapApp/ViewModels/LocationViewModel.swift
+++ SwiftfulMapApp/ViewModels/LocationViewModel.swift
@@ -5,13 +5,31 @@
 //  Created by Jason Cheladyn on 2022/07/14.
 //
 
-import Foundation
+import SwiftUI
+import MapKit
 
 class LocationsViewModel: ObservableObject {
     
     @Published var locations: [Location]
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
-        self.locations = LocationsDataService.locations
+        let locations = LocationsDataService.locations
+        self.locations = locations
+        self.mapLocation = locations.first!
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(center: location.coordinates, span: mapSpan)
+        }
     }
 }
